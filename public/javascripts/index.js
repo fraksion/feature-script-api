2ab2bb936a307e2edfd154967aba121de8ea334f
fraksion <cap.kryglui@gmail.com>
2019-07-08 12:46:47 +0300

    hard test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -479,11 +479,12 @@
     }
 
     function TEST(){
+        getCurrentMicroversion();
         var dfd = $.Deferred();
             $.ajax("/api/test",{
                 type: "POST",
                 dataType: "json",
-                data:test, 
+                data:testCustomFeature, 
                 contentType: "application/json",
                 Accept:'application/vnd.onshape.v1+json',
                 complete: function() {
@@ -499,78 +500,80 @@
               });
               return dfd.resolve();
     }
-    const testCustomFeature = "{\"feature\" : {"+
-    "\"type\" : 134,"+
-   " \"typeName\" : \"BTMFeature\","+
-    "\"message\" : {"+
-      "\"featureType\" : \"myFeature\","+
-      "\"featureId\" : \"FfNjGFbqLFOhBTW_1\","+
-     " \"name\" : \"Helix around the curve 1\","+
-      "\"parameters\" : [ {"+
-        "\"type\" : 148,"+
-        "\"typeName\" : \"BTMParameterQueryList\","+
+    const testCustomFeature = "{"+
+        "\"feature\" : {"+
+       " \"type\" : 134,"+
+        "\"typeName\" : \"BTMFeature\","+
         "\"message\" : {"+
-          "\"queries\" : [ {"+
-            "\"type\" : 138,"+
-            "\"typeName\" : \"BTMIndividualQuery\","+
+          "\"featureType\" : \"myFeature\","+
+          "\"featureId\" : \"F72QEJFw4m6i2Fh_0\","+
+          "\"name\" : \"Helix around the curve 1\","+
+          "\"parameters\" : [ {"+
+            "\"type\" : 148,"+
+           " \"typeName\" : \"BTMParameterQueryList\","+
             "\"message\" : {"+
-             " \"geometryIds\" : [ \"JFB\" ],"+
+             " \"queries\" : [ {"+
+               " \"type\" : 138,"+
+               " \"typeName\" : \"BTMIndividualQuery\","+
+                "\"message\" : {"+
+                "  \"geometryIds\" : [ \"JFB\" ],"+
+                  "\"hasUserCode\" : false,"+
+                 " \"nodeId\" : \"FdpOUOU1jJ3DmQo\""+
+               " }"+
+              "} ],"+
+              "\"parameterId\" : \"curve\","+
               "\"hasUserCode\" : false,"+
-              "\"nodeId\" : \"FUWpkv7ZWsYBGlW\""+
+              "\"nodeId\" : \"JuN2B/4FU8DYaymZ\""+
+            "}"+
+          "}, {"+
+            "\"type\" : 147,"+
+            "\"typeName\" : \"BTMParameterQuantity\","+
+            "\"message\" : {"+
+              "\"units\" : \"\","+
+             "\"value\" : 0.0,"+
+             " \"expression\" : \"25 mm\","+
+              "\"isInteger\" : false,"+
+              "\"parameterId\" : \"radius\","+
+              "\"hasUserCode\" : false,"+
+              "\"nodeId\" : \"JS0zLyL8L6P4T3Ji\""+
            " }"+
+         " }, {"+
+            "\"type\" : 147,"+
+           "\"typeName\" : \"BTMParameterQuantity\","+
+            "\"message\" : {"+
+              "\"units\" : \"\","+
+              "\"value\" : 0.0,"+
+              "\"expression\" : \"8\","+
+              "\"isInteger\" : true,"+
+             " \"parameterId\" : \"revolutions\","+
+             "\"hasUserCode\" : false,"+
+            " \"nodeId\" : \"HF+iLwj0pHP1PKDh\""+
+            "}"+
+          "}, {"+
+            "\"type\" : 144,"+
+            "\"typeName\" : \"BTMParameterBoolean\","+
+           " \"message\" : {"+
+             " \"value\" : false,"+
+             " \"parameterId\" : \"isConnected\","+
+             " \"hasUserCode\" : false,"+
+              "\"nodeId\" : \"nWtVxCim6s0MGa+g\""+
+            "}"+
          " } ],"+
-          "\"parameterId\" : \"curve\","+
-          "\"hasUserCode\" : false,"+
-          "\"nodeId\" : \"2XjoRIgJQgneqb+X\""+
-        "}"+
-      "}, {"+
-        "\"type\" : 147,"+
-        "\"typeName\" : \"BTMParameterQuantity\","+
-        "\"message\" : {"+
-          "\"units\" : \"\","+
-          "\"value\" : 0.0,"+
-          "\"expression\" : \"25 mm\","+
-          "\"isInteger\" : false,"+
-          "\"parameterId\" : \"radius\","+
-          "\"hasUserCode\" : false,"+
-          "\"nodeId\" : \"ScTS3UvTnjkbLe/l\""+
+          "\"suppressed\" : false,"+
+          "\"namespace\" : \"ed1399b3f2457d65abf1c8426::m305a547f9d5ccdefeccc3ed5\","+
+          "\"subFeatures\" : [ ],"+
+          "\"returnAfterSubfeatures\" : false,"+
+          "\"suppressionState\" : {"+
+          "\"type\" : 0"+
+         " },"+
+        "  \"hasUserCode\" : false,"+
+          "\"nodeId\" : \"MKfAioS6LKOPwpQfJ\""+
        " }"+
-      "}, {"+
-        "\"type\" : 147,"+
-        "\"typeName\" : \"BTMParameterQuantity\","+
-        "\"message\" : {"+
-          "\"units\" : \"\","+
-          "\"value\" : 0.0,"+
-          "\"expression\" : \"2\","+
-          "\"isInteger\" : true,"+
-          "\"parameterId\" : \"revolutions\","+
-          "\"hasUserCode\" : false,"+
-          "\"nodeId\" : \"M3z68moPJHY7rbYDQ\""+
-       " }"+
-      "}, {"+
-        "\"type\" : 144,"+
-       " \"typeName\" : \"BTMParameterBoolean\","+
-        "\"message\" : {"+
-         " \"value\" : false,"+
-          "\"parameterId\" : \"isConnected\","+
-          "\"hasUserCode\" : false,"+
-          "\"nodeId\" : \"MgQ1+tAOoloQKinFP\""+
-        "}"+
-      "} ],"+
-      "\"suppressed\" : false,"+
-      "\"namespace\" : \"ed1399b3f2457d65abf1c8426::m305a547f9d5ccdefeccc3ed5\","+
-      "\"subFeatures\" : [ ],"+
-      "\"returnAfterSubfeatures\" : false,"+
-      "\"suppressionState\" : {"+
-        "\"type\" : 0"+
       "},"+
-      "\"hasUserCode\" : false,"+
-      "\"nodeId\" : \"MDD3Msm33pTp1fwSO\""+
-    "}"+
-  "} ,"+
-  "\"serializationVersion\": \"1.1.17\","+
-  "\"sourceMicroversion\": \"052c2be70b18c78ff91f4bec\""+
-  "}";
+      "\"serializationVersion\": \"1.1.17\","+
+      "\"sourceMicroversion\": \"" + microversion + "\","+
+      "\"rejectMicroversionSkew\": \"false\""+
+    "}"
 
 
   const test = "{"+
