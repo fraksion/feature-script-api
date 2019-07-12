4044bf5c3e136b7afb55e76b4aa293884313bdfe
fraksion <cap.kryglui@gmail.com>
2019-07-12 11:00:51 +0300

    dashed lines try

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -574,7 +574,8 @@
                 geometry.vertices.push(new THREE.Vector3(point[0], point[1], point[2]));
             });
             let line = new THREE.Line(geometry, material);
-            geometry.computeLineDistances();
+            if (isConstruction){line.computeLineDistances();}
+            
 
             testcSys.add(line);
             //THREE.GeometryUtils.center(geometry);
