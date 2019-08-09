d09cfd3bbdd48d947c345c939802c513af2c00ee
fraksion <cap.kryglui@gmail.com>
2019-08-09 14:21:25 +0300

    kjh

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -253,6 +253,7 @@
             
         }
         SplinePointSctiptQuery = { "key" : "csvData", "value" : csvPointsArray };
+        console.log(`test = ${SplinePointSctipt}`);
     }
 
     // Functions to support loading list of models to view ...
