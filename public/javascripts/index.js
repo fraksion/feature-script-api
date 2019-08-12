606973a845798142611e1861fc89eb9d97749339
fraksion <cap.kryglui@gmail.com>
2019-08-12 14:55:44 +0300

    Еуые

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -692,8 +692,8 @@
                 let line = new THREE.Line(geometry, material);
                 if (i == 0) {
                     const axes = new THREE.AxesHelper();
-                    //axes.material.depthTest = false;
-                    axes.renderOrder = 0;
+                    axes.material.depthTest = false;
+                    axes.renderOrder = 1;
                     scene.add(axes);
                     loadedModels.push(axes);
                 }
